59ec9643cec3789aabeab95434bc630ff1a098b8
Arthur de belleville* <a2belleville@gmail.com>
2021-03-10 17:17:50 +0100

    creation entité + dev template + debut form + fixtures

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use Doctrine\ORM\Mapping\Id;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class BlogController extends AbstractController
 {
@@ -22,26 +27,79 @@ class BlogController extends AbstractController
 
     }
 
+
+    /**
+ * 
+ * @route("/blog/new", name="blog_create")
+ * 
+ */
+public function create(Request $Request): Response
+{
+
+    dump($Request);
+
+    return $this->render('blog/create.html.twig');
+}
+
+
+
+
+
+
+/*
+            Pour selectionner des données dans une table SQL, nous devons absolument avoir accès à la classe Repository de l'entité correspondante 
+            Un Repository est une classe permettant uniquement d'executer des requetes de selection en BDD (SELECT)
+            Nous devons donc accéder au repository de l'netité Article au sein de notre controller  
+
+            On appel l'ORM doctrine (getDoctrine()), puis on importe le repositoritory de la classe Article grace à la méthode getRepository()
+            $repo est un objet issu de la classe ArticleRepository
+            cet objet contient des méthodes permettant d'executer des requetes de selections
+            findAll() : méthode issue de la classe ArticleRepository permettant de selectionner l'ensemble de la table SQL 'Article'
+        */
+// on envoie sur le template, les articles selectionnés en BDD afin de pouvoir les afficher dynamiquement sur le template à l'aide du langage Twig
+
+
+
     /**
      * 
      * Methode permettant d'afficher tout la liste des articles stockés en BDD
      * 
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        //$repo = $this->getdoctrine()->getRepository(Article::class);
+
+        dump($repo);
+
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'title' => 'Listes des Articles',
+            'articles' => $articles
         ]);
     }
 
     /**
      * methode permettant dafficher le details d'un articles 
      * 
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(): Response
+    public function show(Article $article): Response
     {
-        return $this->render('blog/show.html.twig');
+       // $repoArticles = $this->getdoctrine()->getRepository(Article::class);
+
+       // dump($repoArticles);
+
+       // dump($id);
+
+        //$article = $repoArticles->find($id);
+
+        dump($article);
+
+        return $this->render('blog/show.html.twig', [ 'articleTwig' =>$article ]); 
+
+           
     }
+
 }
